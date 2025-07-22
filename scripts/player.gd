4879aa922c896f8265bd76b8c27153c17f81b1fd
Vetle Olavesen <vetleolavesen@gmail.com>
2025-07-22 19:18:03 +0200

    Move main_menu and player scenes

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,22 +2,35 @@ extends CharacterBody2D
 
 enum MOVEMENT_STATES {IDLE, IDLE_UP, WALKING, WALKING_UP}
 
-const SPEED = 300.0
-const DECEL_SPEED = 50.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var DECEL_SPEED = 50.0
+@export var AIM_CIRCLE_RADIUS = 30.0
+
+const max_aim_dist_mult = 150.0
 
 var animation_sprite: AnimatedSprite2D
+var coin: RigidBody2D
+
+# Aiming vars
+var aim_circle: Sprite2D
+var aim_direction: Vector2
+var aim_strength = 0.5
+var toss_location = Vector2(0,0)
 
 func _ready() -> void:
+	coin = $Coin
 	animation_sprite = $AnimatedSprite2D
+	aim_circle = $AimCircle
+	aim_circle.position = Vector2(1,0)
+	
 	
 func _process(delta: float) -> void:
-	pass
+	aim_point_update()
+	queue_redraw()
+	
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Vector2(Input.get_axis("ui_right", "ui_left"), Input.get_axis("ui_down", "ui_up")).normalized()
+	var direction := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
 	if direction:
 		velocity = (direction * SPEED)
 	else:
@@ -26,7 +39,25 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func aim_point_update() -> void:
+	var aim_direction = get_local_mouse_position().normalized()
+	var aim_point = (aim_direction * AIM_CIRCLE_RADIUS)
+	
+	if Input.is_action_pressed("build_strength"):
+		aim_point = aim_point.move_toward(get_local_mouse_position().normalized() * max_aim_dist_mult, aim_strength)
+		toss_location = aim_point
+		aim_strength += 0.2
+		
+	elif Input.is_action_just_released("build_strength"):
+		aim_strength = 0.5
+		coin.throw()
+		print(toss_location)
+	
+	aim_circle.position = aim_point
+
+func _draw() -> void:
+	if toss_location:
+		draw_circle(toss_location, 2.0, Color.RED)
 
 func animation_handler():
-	if (velocity.x < 0):
-		animation_sprite.play()
+	pass
