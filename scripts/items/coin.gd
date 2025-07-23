13ff2e4fd7301d6b9b71801304b7ee35dbfd273b
Vetle Olavesen <vetleolavesen@gmail.com>
2025-07-23 19:57:44 +0200

    Rebuild coin into area2d, add coin behaviour, throw, pickup, and hit sound

--- scripts/items/coin.gd
+++ scripts/items/coin.gd
@@ -1,46 +1,69 @@
-extends RigidBody2D
+extends Area2D
 
-enum COIN_STATES {EQUIPPED, FLYING, PICKABLE}
+enum COIN_STATES {FLYING, PICKABLE}
 const ACTIONS := ["throw", "pick_up"] # Only to keep track of available actions
 
-var particles: CPUParticles2D
-var animated_sprite: AnimatedSprite2D
+@onready var particles: CPUParticles2D = $CPUParticles2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var hit_sound_player: AudioStreamPlayer2D = $HitSound
+
+var destination: Vector2
+var duration: float = 0.3
+var tween: Tween
 
 @export var _next_state: COIN_STATES
 @export var _current_state: COIN_STATES
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	particles = $CPUParticles2D
-	animated_sprite = $AnimatedSprite2D
-	
+	collision_shape.disabled = true
 	particles.emitting = false
 	
-	var _next_state = COIN_STATES.EQUIPPED
-	var _current_state = _next_state
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.finished.connect(_on_tween_finish)
 	
+	_next_state = COIN_STATES.FLYING
+	_current_state = _next_state
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if _current_state != _next_state:
 		_current_state = _next_state
+		print(COIN_STATES.keys()[_current_state])
 	
-	particle_process()
-
-func _physics_process(delta: float) -> void:
-	pass
+	_state_property_handler()
 
 
-func particle_process() -> void:
-	if _current_state == COIN_STATES.PICKABLE:
-		particles.emitting = true
-	else:
-		particles.emitting = false
+func _state_property_handler():
+	match _current_state:
+		COIN_STATES.FLYING:
+			collision_shape.disabled = false
+			particles.emitting = false
+		
+		COIN_STATES.PICKABLE:
+			particles.emitting = true
+
+func _on_tween_finish():
+	tween.kill()
+	_next_state = COIN_STATES.PICKABLE
 
 # METHODS TO BE CALLED BY PLAYER ACTIONS
-func throw() -> void:
-	_next_state = COIN_STATES.FLYING
-	print("COIN IS THROWN")
+func throw(coin_dest: Vector2) -> void:
+	tween.tween_property(self, "position", coin_dest, duration)
 
-func pick_up() -> void:
-	_next_state = COIN_STATES.EQUIPPED
+func pick_up() -> bool:
+	if _current_state == COIN_STATES.PICKABLE:
+		queue_free()
+		return true
+	return false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if tween and tween.is_valid():
+		tween.kill()
+	
+	hit_sound_player.play()
+	_next_state = COIN_STATES.PICKABLE
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,30 +2,37 @@ extends CharacterBody2D
 
 enum MOVEMENT_STATES {IDLE, IDLE_UP, WALKING, WALKING_UP}
 
-@export var SPEED = 300.0
-@export var DECEL_SPEED = 50.0
-@export var AIM_CIRCLE_RADIUS = 30.0
-
 const max_aim_dist_mult = 150.0
 
-var animation_sprite: AnimatedSprite2D
-var coin: RigidBody2D
+## Variables
+
+# Eported vars
+@export var SPEED := 150.0
+@export var DECEL_SPEED := 50.0
+@export var AIM_CIRCLE_RADIUS := 30.0
+
+# Nodes
+@onready var aim_circle: Sprite2D = $AimCircle
+@onready var animation_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var pick_up_range: Area2D = $PickUpRange
+@onready var coin_scene := load("res://scenes/items/coin.tscn")
+var coin: Area2D
+
+# Coin available?
+var has_coin: bool
 
 # Aiming vars
-var aim_circle: Sprite2D
 var aim_direction: Vector2
-var aim_strength = 0.5
-var toss_location = Vector2(0,0)
+var aim_strength := 0.5
+var toss_location := Vector2(0,0)
 
 func _ready() -> void:
-	coin = $Coin
-	animation_sprite = $AnimatedSprite2D
-	aim_circle = $AimCircle
 	aim_circle.position = Vector2(1,0)
-	
-	
+	has_coin = true
+
 func _process(delta: float) -> void:
 	aim_point_update()
+	coin_pickup()
 	queue_redraw()
 	
 
@@ -39,25 +46,49 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+
+func coin_pickup() -> void:
+	if not coin:
+		return
+	if coin not in pick_up_range.get_overlapping_areas():
+		return
+	
+	if Input.is_action_just_pressed("pick_up"):
+		if coin.pick_up():
+			coin = null
+			has_coin = true
+
+
+
+
 func aim_point_update() -> void:
 	var aim_direction = get_local_mouse_position().normalized()
 	var aim_point = (aim_direction * AIM_CIRCLE_RADIUS)
 	
-	if Input.is_action_pressed("build_strength"):
+	if not has_coin and Input.is_action_pressed("build_strength"):
+		print("YOU HAVE NO COIN TO SHOOT")
+	
+	elif Input.is_action_pressed("build_strength") and has_coin:
 		aim_point = aim_point.move_toward(get_local_mouse_position().normalized() * max_aim_dist_mult, aim_strength)
 		toss_location = aim_point
 		aim_strength += 0.2
-		
-	elif Input.is_action_just_released("build_strength"):
+	
+	elif Input.is_action_just_released("build_strength") and has_coin:
 		aim_strength = 0.5
-		coin.throw()
-		print(toss_location)
+		coin = coin_scene.instantiate()
+		get_tree().root.add_child(coin)
+		coin.transform = transform
+		coin.throw(to_global(toss_location))
+		has_coin = false
 	
 	aim_circle.position = aim_point
 
-func _draw() -> void:
-	if toss_location:
-		draw_circle(toss_location, 2.0, Color.RED)
 
 func animation_handler():
 	pass
+
+
+## DEBUG
+func _draw() -> void:
+	if toss_location:
+		draw_circle(toss_location, 2.0, Color.RED)
