43664554f7819d3ba3e40a1013078f25fe052c9c
Vetle Olavesen <vetleolavesen@gmail.com>
2025-07-24 15:41:56 +0200

    Add audio_player to lever, plays on coin hit

--- scenes/interactables/lever.gd
+++ scenes/interactables/lever.gd
@@ -6,6 +6,7 @@ signal activate
 
 ## Nodes
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 ## States
 var _current_active_state: bool = false
@@ -17,14 +18,16 @@ var id: int
 func _process(delta: float) -> void:
 	if _current_active_state != _next_active_state:
 		_current_active_state = _next_active_state
-		_animation_handler()
+		_animation_audio_handler()
 
-func _animation_handler():
+func _animation_audio_handler():
 	match _current_active_state:
 		true:
 			activate.emit()
+			audio_player.play()
 			animation.play("activate")
 		false:
+			audio_player.play()
 			animation.play("de_activate")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
@@ -32,4 +35,3 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		return
 	
 	_next_active_state = !_current_active_state
-	print(_next_active_state)
