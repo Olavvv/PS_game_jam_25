9582970e5640e79ecc3560cde225d88e0239189b
Vetle Olavesen <vetleolavesen@gmail.com>
2025-07-24 16:36:24 +0200

    Add lever logic to play individual sfxs for de-activate and activare

--- scenes/interactables/lever.gd
+++ scenes/interactables/lever.gd
@@ -8,6 +8,10 @@ signal activate
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+## Audios
+var activate_WAV: AudioStreamWAV = load("res://assets/audio/sfx/lever_activate.wav")
+var deactivate_WAV: AudioStreamWAV = load("res://assets/audio/sfx/lever_activate.wav")
+
 ## States
 var _current_active_state: bool = false
 var _next_active_state: bool = false
@@ -24,9 +28,11 @@ func _animation_audio_handler():
 	match _current_active_state:
 		true:
 			activate.emit()
+			audio_player.stream = activate_WAV
 			audio_player.play()
 			animation.play("activate")
 		false:
+			audio_player.stream = deactivate_WAV
 			audio_player.play()
 			animation.play("de_activate")
 
